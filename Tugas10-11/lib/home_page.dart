bb6c595f585c91f49f63fcfadc843fd03f9b2f95
Gals96 <2310631250059@student.unsika.ac.id>
2025-11-26 15:28:12 +0700

    Add files via upload

---
+++ Tugas10-11/lib/home_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(16),
+        children: const [
+          Center(
+            child: Text(
+              'Welcome to the Home Page!',
+              style: TextStyle(fontSize: 24),
+            ),
+          ),
+          SizedBox(height: 20),
+          
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text('Profil'),
+            trailing: Icon(Icons.arrow_forward_ios, size: 16),
+          ),
+
+          ListTile(
+            leading: Icon(Icons.logout),
+            title: Text('Keluar'),
+            trailing: Icon(Icons.arrow_forward_ios, size: 16),
+          ),
+        ],
+      ),
+    );
+  }
+}
